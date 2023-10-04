2c8ea8debf539c2b3fd12ae86004188c18faee1c
sthefanyk <sthefanykmbrl@gmail.com>
2023-10-04 16:32:16 -0300

    add findById

--- chamaApi.js
+++ chamaApi.js
@@ -1,80 +1,177 @@
-// Obtém uma referência ao elemento da tabela onde os personagens serão exibidos.
-const corpoTabelaPersonagens = document.getElementById('corpoTabelaPersonagens');
+document.addEventListener("DOMContentLoaded", function () {
+    buscarDadosEPreencherTabela();
+});
 
-// Função que busca os dados da API e preenche a tabela com os usuarios.
+const corpoTabelaPersonagens = document.getElementById(
+    "corpoTabelaPersonagens"
+);
 
 function buscarDadosEPreencherTabela() {
-    // Faz uma requisição GET para a API.
-    axios.get('http://infopguaifpr.com.br:3052/listarTodosUsuarios')
-        .then(response => {
-            console.log(response)
-
-            // Obtém a lista de usuários da resposta.
+    axios
+        .get("http://localhost:3000/listarTodosUsuarios")
+        .then((response) => {
             const usuarios = response.data.usuarios;
-
-            // Chama a função para preencher a tabela com os usuários.
             preencherTabela(usuarios);
         })
-        .catch(error => {
-            // Em caso de erro, exibe uma mensagem de erro no console.
-            console.error('Error fetching character data:', error);
+        .catch((error) => {
+            console.error("Error fetching character data:", error);
         });
 }
 
-// Função que preenche a tabela com os dados dos usuários.
 function preencherTabela(usuarios) {
-    // Para cada usuário na lista...
-    usuarios.forEach(usuario => {
+
+    // clear table
+    while (corpoTabelaPersonagens.firstChild) {
+        corpoTabelaPersonagens.removeChild(corpoTabelaPersonagens.firstChild);
+    }
+    
+    usuarios.forEach((usuario) => {
         // Cria uma nova linha na tabela.
-        const linha = document.createElement('tr');
+        const linha = document.createElement("tr");
 
         // Cria células para cada dado do usuário e insere o texto.
-        const idCelula = document.createElement('td');
+        const idCelula = document.createElement("td");
         idCelula.textContent = usuario.id;
         linha.appendChild(idCelula);
 
         // Cria células para cada dado do usuário e insere o texto.
-        const nomeCelula = document.createElement('td');
+        const nomeCelula = document.createElement("td");
         nomeCelula.textContent = usuario.nome;
         linha.appendChild(nomeCelula);
 
-        const emailCelula = document.createElement('td');
+        const emailCelula = document.createElement("td");
         emailCelula.textContent = usuario.email;
         linha.appendChild(emailCelula);
 
-        const disciplinaCelula = document.createElement('td');
+        const disciplinaCelula = document.createElement("td");
         disciplinaCelula.textContent = usuario.disciplina;
         linha.appendChild(disciplinaCelula);
 
         // Cria células para os botões de editar e excluir.
-        const acoesCelula = document.createElement('td');
-        const editarBotao = document.createElement('a');
-        editarBotao.href = '#';
-        editarBotao.className = 'btn btn-primary btn-edit';
-        editarBotao.textContent = 'Editar';
+        const acoesCelula = document.createElement("td");
+        const editarBotao = document.createElement("a");
+        editarBotao.href = "#";
+        editarBotao.className = "btn btn-primary btn-edit";
+        editarBotao.textContent = "Editar";
         editarBotao.dataset.id = usuario.id;
         acoesCelula.appendChild(editarBotao);
 
-        const excluirBotao = document.createElement('a');
-        excluirBotao.href = '#';
-        excluirBotao.className = 'btn btn-danger btn-delete';
-        excluirBotao.textContent = 'Excluir';
+        const excluirBotao = document.createElement("a");
+        excluirBotao.href = "#";
+        excluirBotao.className = "btn btn-danger btn-delete";
+        excluirBotao.textContent = "Excluir";
         excluirBotao.dataset.id = usuario.id;
         acoesCelula.appendChild(excluirBotao);
 
         linha.appendChild(acoesCelula);
 
-        // Adiciona a linha preenchida à tabela.
         corpoTabelaPersonagens.appendChild(linha);
     });
 }
 
+// const botaoChamarAPI = document.getElementById("botaoChamarAPI");
+// botaoChamarAPI.addEventListener("click", () => {
+//     buscarDadosEPreencherTabela();
+// });
 
-// Obtém uma referência ao botão que chama a API.
-const botaoChamarAPI = document.getElementById('botaoChamarAPI');
-// Adiciona um ouvinte de evento para o clique no botão.
-botaoChamarAPI.addEventListener('click', () => {
-    // Quando o botão é clicado, chama a função para buscar dados e preencher a tabela.
-    buscarDadosEPreencherTabela();
 
+// CREATE ---------------------------------------------------------------------
+
+document
+    .querySelector("#btnCadastrarUsuario")
+    .addEventListener("click", function () {
+        const nome = document.querySelector("#nome").value;
+        const email = document.querySelector("#email").value;
+        const disciplina = document.querySelector("#disciplina").value;
+        const senha = document.querySelector("#senha").value;
+
+        create(nome, email, disciplina, senha);
+    });
+
+function create(nome, email, disciplina, senha) {
+    console.log("Dados capturados para cadastro:");
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Disciplina:", disciplina);
+    console.log("Senha:", senha);
+
+    const novoUsuario = {
+        nome: nome,
+        email: email,
+        disciplina: disciplina,
+        senha: senha,
+    };
+
+    axios
+        .post("http://localhost:3000/cadastrarUsuario", novoUsuario, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => {
+            console.log("Usuário cadastrado com sucesso:", response.data);
+            $("#cadastrarUsuario").modal("hide");
+            alert("Usuário cadastrado com sucesso");
+            buscarDadosEPreencherTabela();
+        })
+        .catch((error) => {
+            alert("Error ao cadastrar usuário:", error);
+        });
+}
+
+// DELETE ---------------------------------------------------------------------
+
+document.addEventListener("click", function (event) {
+    if (event.target && event.target.classList.contains("btn-delete")) {
+        const id = event.target.dataset.id;
+        deleteUser(id);
+    }
 });
+
+function deleteUser(id) {
+    const confirmacao = confirm("Tem certeza que deseja excluir este usuário?");
+    if (confirmacao) {
+        axios
+            .delete(`http://localhost:3000/deletarUsuario/${id}`)
+            .then((response) => {
+                console.log("Usuário deletado com sucesso!");
+                buscarDadosEPreencherTabela();
+            })
+            .catch((error) => {
+                console.error("Ocoreu um erro ao deletar usuário: ", error);
+            });
+    } else {
+        console.log("Exclusão cancelada pelo usuário");
+    }
+}
+
+
+// UPDATE ---------------------------------------------------------------------
+
+document.addEventListener("click", function (event) {
+    if (event.target && event.target.classList.contains("btn-edit")) {
+        const id = event.target.dataset.id;
+        document.documentElement.dataset.idUsuario = id;
+        findById(id);
+    }
+});
+
+function findById(id) {
+    axios
+        .get(`http://localhost:3000/pegarUsuarioPeloId/${id}`)
+        .then((response) => {
+            const usuario = response.data.usuario;
+
+            document.querySelector('#editarUsuario input[type="text"]').value =
+                usuario.nome;
+            document.querySelector('#editarUsuario input[type="email"]').value =
+                usuario.email;
+            document.querySelector("#editarUsuario textarea").value =
+                usuario.disciplina;
+
+            $("#editarUsuario").modal("show");
+        })
+        .catch((error) => {
+            alert("Ocorreu um erro ao recuperar usuário: ", error);
+        });
+}
